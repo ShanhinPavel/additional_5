6db995a4d8f233b36f7a3b2057d0738b139fc28f
ShanhinPavel <shanMail@tut.by>
2018-03-01 01:04:57 +0300

    Sixth task has solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+   let arrayBrackets = [];
+   let lastElement;
+   let count = 0;
+   let count1 = 0;
+   
+   let newString;
+
+//[['(',')']]
+   while (bracketsConfig[count1]) {
+     newString = bracketsConfig[count1].join("");
+     str += newString;
+     count1++;
+   }
+
+//test brackets
+   while (str[count] !== undefined) {
+    
+     if ((str[count] === '(') ||
+      (str[count] === '[') ||
+      (str[count] === '|') ||
+      (str[count] === '{')) 
+     {
+       arrayBrackets.push(str[count]);
+       if (str[count] === '|') {
+        arrayBrackets.pop();
+      }
+     } else {
+
+       if (arrayBrackets.length === 0) {
+         return false;
+       } else {
+         lastElement = arrayBrackets.pop();
+      
+         if ((lastElement === '(' && str[count] !== ')') ||
+             (lastElement === '[' && str[count] !== ']') ||
+             (lastElement === '{' && str[count] !== '}'))
+             {
+           return false;
+         }
+         }
+       }
+       count++;
+     }
+   if(arrayBrackets.length === 0) {
+     return true;
+} else {return false};
 }
